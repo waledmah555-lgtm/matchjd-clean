9c2cf6b69a17c18a468498b8e7f9149516e845c8
waledmah555-lgtm <waledmah555@gmail.com>
2025-12-27 15:50:36 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
-        {children}
+      <body
+  style={{
+    margin: 0,
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+    background: "#f7f7f8",
+    color: "#111"
+  }}
+>
+
       </body>
     </html>
   );
